dac3b97e749aef136a5dc60d012679d335d26606
joshc0des <joshuahooper3@gmail.com>
2023-01-08 21:52:58 -0600

    2.16

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-// import axios from 'axios'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
