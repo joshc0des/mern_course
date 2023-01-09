aa777478ebfbdf53a89ccca72ae6041c2592029d
joshc0des <joshuahooper3@gmail.com>
2023-01-08 21:51:59 -0600

    2.15

--- part2/phonebook/src/services/names.js
+++ part2/phonebook/src/services/names.js
@@ -13,9 +13,7 @@ const create = newObject => {
     return request.then(response => response.data)
 }
 
-const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response.data)
-}
 
-export default { getAll, create, update }
+const exportedObject = { getAll, create }
+
+export default exportedObject
